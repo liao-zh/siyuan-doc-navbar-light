6630aa84a2b1b47419458e6cee3904326785864d
liao_zhenghan <liaozhenghan@outlook.com>
2025-11-28 17:02:25 +0800

    eventHandler: init inject on layout

--- src/worker/eventHandler.ts
+++ src/worker/eventHandler.ts
@@ -21,9 +21,13 @@ export class EventHandler {
     }
 
     bindHandler() {
+        // 绑定所有事件处理器
         for (let key in this.handlerList) {
             this.plugin.eventBus.on(key as keyof IEventBusMap, this.handlerList[key]);
         }
+
+        // 首次加载时处理已经打开的文档
+        this.handleProtyleAllShowing();
     }
 
     unbindHandler() {
@@ -35,6 +39,20 @@ export class EventHandler {
     async handleProtyle(protyle: IProtyle) {
         const contentInjector = new ContentInjector();
         await contentInjector.apply(protyle, true);
+    }
+
+    async handleProtyleAllShowing() {
+        // 在所有打开的protyle中，仅处理显示着的文档
+        // 两个都需要是因为，文档只能得到id，而editor能得到需要的protyle
+        const allEditor = getAllEditor();
+        const ids = getAllShowingDocId();
+        if (ids != null && ids.length > 0) {
+            for (let editor of allEditor) {
+                if (ids.includes(editor.protyle.block.rootID)) {
+                    await this.handleProtyle(editor.protyle);
+                }
+            }
+        }
     }
 
     async handleLoadedProtyleStatic(event: CustomEvent<IEventBusMap["loaded-protyle-static"]>) {
@@ -54,22 +72,13 @@ export class EventHandler {
         // 仅处理移动、重命名、删除文档事件
         if (cmdType.includes(event.detail.cmd)) {
             logger.logDebug(`ws-main(${cmdType.join('/')})"`, event);
-            // 在所有打开的protyle中，仅处理显示着的文档
-            // 两个都需要是因为，文档只能得到id，而editor能得到需要的protyle
-            const allEditor = getAllEditor();
-            const ids = getAllShowingDocId();
-            if (ids != null && ids.length > 0) {
-                for (let editor of allEditor) {
-                    if (ids.includes(editor.protyle.block.rootID)) {
-                        await this.handleProtyle(editor.protyle);
-                    }
-                }
-            }
+            await this.handleProtyleAllShowing();
         }
     }
 }
 
 // 获取所有显示中的文档id
+// 从文档层级导航插件复制而来
 function getAllShowingDocId(): string[] {
     const elemList = window.document.querySelectorAll("[data-type=wnd] .protyle.fn__flex-1:not(.fn__none) .protyle-background");
     const result = [].map.call(elemList, function (elem: Element) {
