f59434cf73780be5bd81445f86ed23cfdf2a4dd9
liao-zh <liaozhenghan@qq.com>
2026-01-25 22:14:11 +0800

    mod vite config

--- vite.config.ts
+++ vite.config.ts
@@ -36,8 +36,7 @@ export default defineConfig({
                 { src: "./README*.md", dest: "./" },
                 { src: "./plugin.json", dest: "./" },
                 { src: "./preview.png", dest: "./" },
-                { src: "./icon.png", dest: "./" },
-                { src: "./CHANGELOG.md", dest: "./" },
+                { src: "./icon.png", dest: "./" }
             ],
         }),
 
